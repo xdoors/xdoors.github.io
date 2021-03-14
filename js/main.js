27b1ba00da44f3721b15c45e59a9657dd4a08caa
xdoors <80198662+xdoors@users.noreply.github.com>
2021-03-14 10:41:55 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,518 @@
+/*
+ * ALL RIGHTS RESERVED.
+ *
+ * 作者：酷安@_小K同學
+ * 项目开始日期：2020年01月26日
+ * 上次修改时间：2020年03月24日
+ * 开发日志：https://kksan.top/posts/12023
+ *
+ * 开源相关：
+ * Github：https://github.com/Jackie1123/aNavigation
+ * CSSFX：https://cssfx.netlify.com
+ * jQuery：https://jquery.com
+ * slideout：https://slideout.js.org
+ * bootstrap：https://getbootstrap.com
+ * font-awesome：https://fontawesome.com
+ *
+ * 版权所有，请勿删除！
+ */
+
+var storage = window.localStorage;
+var data = storage.data;
+var night = storage.night;
+var bg = storage.bg;
+var closealert = storage.closealert;
+var li = $('.sidenav-btn');
+var blockquote = $('.blockquote');
+
+if (storage.data != undefined) {
+  data = data.split(',');
+  // console.log('已存在localStorage的数据：' + data); //已存在localStorage的数据
+  $('#state a img').attr('src', data[0]); //头图
+  $('.submitButton').css('background-color', data[1]); //按钮bgc
+  $('#inputText').attr('placeholder', data[2]);
+  $('#form').attr('action', data[3]);
+  $('#inputText').attr('name', data[4]);
+  $('#Select').css('color', data[1]);
+  $('.span').css('background-color', data[1]);
+}
+
+if (storage.night != undefined) {
+  night = night.split(',');
+  console.log(night);
+  $('#main').css('background-color', night[0]); //主界面
+  $('#menu').css('background-color', night[1]); //侧栏
+  document.getElementById("night").innerHTML = night[2];
+  li.css('background-color', night[3]);
+  li.css('color', night[4]);
+  blockquote.css('color', night[5]);
+}
+
+if (storage.bg != undefined) {
+  bg = bg.split(',');
+  $('#main').css('background-image', bg[0]);
+}
+
+if (storage.closealert != undefined) {
+  closealert = closealert.split(',');
+  if (closealert[0] == '4.1.1') {
+    $('#alert').remove();
+  }
+}
+
+// rgb to hex
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+  function hex(x) {
+    return ("0" + parseInt(x).toString(16)).slice(-2);
+  }
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+// rgb to hex结束
+
+// 添加书签
+$(function() {
+  var bookmark = {
+    data: [{
+      name: '地图',
+      link: 'https://www.amap.com/',
+      box_shadow: '#F18033',
+      icon: 'https://www.amap.com/favicon.ico',
+    }, {
+      name: '澎湃',
+      link: 'https://www.thepaper.cn/',
+      box_shadow: '#11B063',
+      icon: 'https://www.thepaper.cn/favicon.ico',
+    }, {
+      name: '知乎',
+      link: 'https://www.zhihu.com/',
+      box_shadow: '#FA7199',
+      icon: 'https://www.zhihu.com/favicon.ico',
+    }, {
+      name: 'HOSTLOC',
+      link: 'https://www.hostloc.com/forum-45-1.html',
+      box_shadow: '#F57923',
+      icon: 'https://www.hostloc.com/favicon.ico',
+    }, {
+      name: 'V2EX',
+      link: 'https://www.v2ex.com/go/all4all?p=1',
+      box_shadow: '#2C2C2C',
+      icon: 'https://www.v2ex.com/favicon.ico',
+    }, {
+      name: 'cnBeta',
+      link: 'http://cnbeta.com/',
+      box_shadow: '#37bf4c',
+      icon: 'https://www.cnbeta.com/favicon.ico',
+    }, {
+      name: '京东',
+      link: 'https://jd.com/',
+      box_shadow: '#AC6E2F',
+      icon: 'https://www.jd.com/favicon.ico',
+    }, {
+      name: '淘宝',
+      link: 'https://s.taobao.com/',
+      box_shadow: '#fe7700',
+      icon: 'https://www.taobao.com/favicon.ico',
+    }, {
+      name: '云添加',
+      link: 'http://d.ojpojp.com/',
+      box_shadow: '#62677b',
+      icon: 'img/log.png',
+    }, {
+      name: 'BTbbT',
+      link: 'http://www.52btbtt.com/',
+      box_shadow: '#D7191A',
+      icon: 'http://www.52btbtt.com/favicon.ico',
+    }, {
+      name: '西瓜视频',
+      link: 'https://www.ixigua.com/my/attention/new',
+      box_shadow: '#3279ea',
+      icon: 'https://www.ixigua.com/favicon.ico',
+    }, {
+      name: 'YouTube',
+      link: 'https://www.youtube.com/feed/subscriptions',
+      box_shadow: '#62677b',
+      icon: 'https://www.youtube.com/favicon.ico',
+    }, {
+      name: 'Twitter',
+      link: 'https://twitter.com/home',
+      box_shadow: '#7C5DC7',
+      icon: 'https://twitter.com/favicon.ico',
+    }, {
+      name: 'Instagram',
+      link: 'https://www.instagram.com/',
+      box_shadow: '#00A0E9',
+      icon: 'https://www.instagram.com/favicon.ico',
+    }, {
+      name: 'Facebook',
+      link: 'https://www.facebook.com/',
+      box_shadow: '#62677b',
+      icon: 'https://www.facebook.com/favicon.ico',
+    }, {
+      name: '倍可亲',
+      link: 'https://www.backchina.com/news/',
+      box_shadow: '#7C5DC7',
+      icon: 'img/bkq.ico',
+    }, {
+      name: 'G-Map',
+      link: 'https://www.google.com/maps/@40.7417759,-73.9101737,12.61z',
+      box_shadow: '#00A0E9',
+      icon: 'https://www.google.com/images/branding/product/ico/maps15_bnuw3a_32dp.ico',
+    }, {
+      name: '翻译',
+      link: 'https://translate.google.com',
+      box_shadow: '#7C5DC7',
+      icon: 'https://translate.google.com/favicon.ico',
+    }, {
+      name: '少数派',
+      link: 'https://sspai.com/',
+      box_shadow: '#62677b',
+      icon: 'https://cdn.sspai.com/sspai/assets/img/favicon/icon.ico',
+    }, {
+      name: '吾爱破解',
+      link: 'https://www.52pojie.cn/forum-10-1.html',
+      box_shadow: '#00A0E9',
+      icon: 'http://www.52pojie.cn/favicon.ico',
+    }, {
+      name: '品葱',
+      link: 'https://pincong.rocks/',
+      box_shadow: '#62677b',
+      icon: 'https://pincong.rocks/favicon.ico',
+    }, {
+      name: '福利吧',
+      link: 'https://fuliba2020.net/',
+      box_shadow: '#7C5DC7',
+      icon: 'https://fuliba2020.net/favicon.ico',
+    }, {
+      name: 'MacAPP',
+      link: 'https://www.macapp.so/',
+      box_shadow: '#62677b',
+      icon: 'https://www.macapp.so/img/favicon.ico',
+    }, {
+      name: '端传媒',
+      link: 'https://theinitium.com/',
+      box_shadow: '#00A0E9',
+      icon: 'https://d32kak7w9u5ewj.cloudfront.net/static/bundles/0b9cf7fe9c518887768e6c485764cd12.ico',
+
+
+    }]
+  }
+  for (var i = 0; i < bookmark.data.length; i++) {
+    if (bookmark.data[i].name == 'Via插件') {
+      var addList = '<li class="folder-item col-xs-3 col-sm-2 visible-xs visible-sm"><a target="_blank" href="' + bookmark.data[i].link + '"><div class="folder-item-box"><img class="folder-item-img" style="box-shadow:' + bookmark.data[i].box_shadow + ' 0 14px 12px -6px" src="' + bookmark.data[i].icon + '" /><p>' + bookmark.data[i].name + '</p></div></a></li>'
+    } else {
+      var addList = '<li class="folder-item col-xs-3 col-sm-2"><a target="_blank" href="' + bookmark.data[i].link + '"><div class="folder-item-box"><img class="folder-item-img" style="box-shadow:' + bookmark.data[i].box_shadow + ' 0 14px 12px -8px" src="' + bookmark.data[i].icon + '" /><p>' + bookmark.data[i].name + '</p></div></a></li>'
+    }
+    $('#folder ul').append(addList);
+  }
+})
+// 添加书签结束
+
+// 搜索相关
+$(function() {
+  var search = {
+    data: [{
+      name: 'google',
+      icon: 'img/google-xs.png',
+      searchlink: 'https://www.google.com/search',
+      searchname: 'q',
+      color: '#4285f4',
+      placeholder: 'Google搜索...'
+    }, {
+      name: 'baidu',
+      icon: 'img/baidu-xs.png',
+      searchlink: 'https://www.baidu.com/s',
+      searchname: 'wd',
+      color: '#3245df',
+      placeholder: '百度一下...'
+    }, {
+      name: 'bing',
+      icon: 'img/bing-xs.png',
+      searchlink: 'https://cn.bing.com/search',
+      searchname: 'q',
+      color: '#00868B',
+      placeholder: 'Bing搜索...'
+    }, {
+      name: 'duckduckgo',
+      icon: 'img/duckduckgo-xs.png',
+      searchlink: 'https://duckduckgo.com',
+      searchname: 'q',
+      color: '#de5833',
+      placeholder: '嘎嘎嘎...'
+    }, {
+      name: 'jingdong',
+      icon: 'img/jingdong-xs.png',
+      searchlink: 'https://search.jd.com/Search',
+      searchname: 'keyword',
+      color: '#F30213',
+      placeholder: '多，快，好，省...'
+    }, {
+      name: 'taobao',
+      icon: 'img/taobao-xs.png',
+      searchlink: 'https://s.taobao.com/search',
+      searchname: 'q',
+      color: '#FF5B00',
+      placeholder: '淘，我喜欢...'
+    }, {
+      name: 'wechat',
+      icon: 'img/wechat-xs.png',
+      searchlink: 'https://weixin.sogou.com/weixin',
+      searchname: 'query',
+      color: '#2ca43a',
+      placeholder: '搜微信文章...'
+    }, {
+      name: 'bilibili',
+      icon: 'img/bilibili-xs.png',
+      searchlink: 'https://search.bilibili.com/all',
+      searchname: 'keyword',
+      color: '#e47494',
+      placeholder: 'b站是一个学习网站...'
+    }, {
+      name: 'toutiao',
+      icon: 'img/toutiao-xs.png',
+      searchlink: 'https://m.toutiao.com/search',
+      searchname: 'keyword',
+      color: '#ed2f28',
+      placeholder: '搜今日头条...'
+    }, {
+      name: 'weibo',
+      icon: 'img/weibo-xs.png',
+      searchlink: 'https://s.weibo.com/weibo',
+      searchname: 'q',
+      color: '#e6162d',
+      placeholder: '搜微博...'
+    }, {
+      name: 'zhihu',
+      icon: 'img/zhihu-xs.png',
+      searchlink: 'https://www.zhihu.com/search',
+      searchname: 'q',
+      color: '#1087eb',
+      placeholder: '我们都是有问题的人...'
+    }, {
+      name: 'kuaidi',
+      icon: 'img/kuaidi-xs.png',
+      searchlink: 'https://m.kuaidi100.com/result.jsp',
+      searchname: 'nu',
+      color: '#317EE7',
+      placeholder: '搜快递...'
+    }]
+  }
+  for (var i = 0; i < search.data.length; i++) { //添加搜索按钮
+    var addList = '<li class="folder-item2 col-xs-3 col-sm-2"> <a href="#"> <div class="folder-item-box2"> <img id="' + search.data[i].name + '" class="folder-item-img2" src="' + search.data[i].icon + '" /> </div> </a> </li> '
+    $('.nav ul').append(addList);
+  }
+
+  // 切换搜索引擎
+  $(document).click(function(e) {
+    var id = e.target.id;
+    for (var i = 0; i < search.data.length; i++) {
+      if (id == search.data[i].name) {
+        document.getElementById("state").innerHTML = "<a href='folder://'><img style='width:300px;' src='img/" + search.data[i].name + ".png'></a>";
+        $('#submitButton').css('background-color', search.data[i].color); //按钮bg
+        $('#Select').css('color', search.data[i].color); //选择器
+        $('#nav').css('display', 'none');
+        $('#folder').css('display', 'block');
+        document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>";
+        $("#inputText").attr("placeholder", search.data[i].placeholder);
+        $("#form").attr("action", search.data[i].searchlink);
+        $("#inputText").attr("name", search.data[i].searchname);
+
+        //存入用户数据
+        var searchPho = $('#state a img').attr("src"); //搜索引擎头图
+        var color = rgb2hex($('.submitButton').css('background-color')); //搜索按钮颜色和搜索框四边颜色
+        var searchL = $('#form').attr("action"); //searchlink
+        var searchN = $('#inputText').attr("name"); //searchname
+        var placeholder = $('#inputText').attr('placeholder');
+        storage.data = [searchPho, color, placeholder, searchL, searchN] //记录用户数据
+        // console.log('新存入localStorage的数据：' + storage.data); //新存入localStorage的数据
+        break;
+      }
+    }
+  })
+  // 搜索相关结束
+
+  // 夜间模式
+  $('#night').click(function() {
+    if (rgb2hex($('#main').css('background-color')) == '#ffffff') {
+      $('#main').css('background-color', '#2f2f33'); //主界面
+      $('#menu').css('background-color', '#5C5C5C'); //侧栏
+      document.getElementById("night").innerHTML = "日间模式";
+      li.css({
+        "background-color": "#575757",
+        "color": "#DBDBDB"
+      });
+      blockquote.css('color', '#DBDBDB');
+    } else if (rgb2hex($('#main').css('background-color')) == '#2f2f33') {
+      $('#main').css('background-color', '#ffffff'); //主界面
+      $('#menu').css('background-color', '#EDEDED'); //侧栏
+      document.getElementById("night").innerHTML = "夜间模式";
+      li.css({
+        "background-color": "#E3E3E3",
+        "color": "black"
+      });
+      blockquote.css('color', 'black');
+    }
+    var mainbg = rgb2hex($('#main').css('background-color'));
+    var menubg = rgb2hex($('#menu').css('background-color'));
+    var inner = document.getElementById("night").innerHTML;
+    var libg = rgb2hex(li.css('background-color'));
+    var lico = rgb2hex(li.css('color'));
+    var blockquoteco = rgb2hex(blockquote.css('color'));
+    // storage.night = [mainbg, menubg, inner, libg, lico, blockquoteco];
+    // localStorage.clear()
+  })
+  // 夜间模式结束
+
+  // 更新提示框
+  $('#closealert').click(function() {
+    var version = '4.1.1';
+    localStorage.closealert = [version];
+  })
+  // 更新提示框结束
+
+})
+
+// 显示/干掉壁纸
+var width = $(document).width();
+$('#bg').click(function() {
+  width < 768 ? ($('#main').css('background-image') == 'none' ? ($('#main').css('background-image', 'url(img/bg-xs.jpg)')) : ($('#main').css('background-image', 'none'))) : $('#main').css('background-image') == 'none' ? ($('#main').css('background-image', 'url(img/bg.jpg)')) : ($('#main').css('background-image', 'none'));
+  var background = $('#main').css('background-image');
+  storage.bg = [background];
+})
+
+//检查搜索框是否为空
+function check() {
+  var o = document.getElementById("inputText");
+  var v = o.value;
+  v = v.replace(/[ ]/g, "");
+  if (v == '') {
+    return false;
+  }
+}
+//检查搜索框是否为空结束
+
+//title问候语
+var d = new Date();
+var time = d.getHours();
+if (time < 24) {
+  document.getElementById("title").innerHTML = "CJ Directory | Good evening";
+}
+if (time < 18) {
+  document.getElementById("title").innerHTML = "CJ Directory | Good afternoon";
+}
+if (time < 12) {
+  document.getElementById("title").innerHTML = "CJ Directory | Good morning";
+}
+if (time < 5) {
+  document.getElementById("title").innerHTML = "CJ Directory | Stay up late again";
+}
+//title问候语结束
+
+//导航、引擎选择器
+function select() {
+  $('#folder').css('display') == 'block' ? ($('#folder').css('display', 'none'), $('#nav').css('display', 'block'), document.getElementById("Select").innerHTML = "<hr>搜索引擎 <img src='img/search-change.svg?v=2ae7ab8'>") : ($('#nav').css('display', 'none'), $('#folder').css('display', 'block'), document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>");
+}
+
+// 天气插件
+(function(a, h, g, f, e, d, c, b) {
+  b = function() {
+    d = h.createElement(g);
+    c = h.getElementsByTagName(g)[0];
+    d.src = e;
+    d.charset = "utf-8";
+    d.async = 1;
+    c.parentNode.insertBefore(d, c)
+  };
+  a["SeniverseWeatherWidgetObject"] = f;
+  a[f] || (a[f] = function() {
+    (a[f].q = a[f].q || []).push(arguments)
+  });
+  a[f].l = +new Date();
+  if (a.attachEvent) {
+    a.attachEvent("onload", b)
+  } else {
+    a.addEventListener("load", b, false)
+  }
+}(window, document, "script", "SeniverseWeatherWidget", "//cdn.sencdn.com/widget2/static/js/bundle.js?t=" + parseInt((new Date().getTime() / 100000000).toString(), 10)));
+window.SeniverseWeatherWidget('show', {
+  flavor: "bubble",
+  location: "WX4FBXXFKE4F",
+  geolocation: true,
+  language: "auto",
+  unit: "c",
+  theme: "auto",
+  token: "e14489a8-9a7e-477d-9c6c-b4b390175cca",
+  hover: "enabled",
+  container: "tp-weather-widget"
+})
+
+// 搜索提示词
+class searchHint {
+  constructor() {
+    this.search = inputText;
+    this.list = list;
+    this.body = document.body;
+    this.init();
+  };
+  init() {
+    this.watchInput();
+  };
+  watchInput() {
+    this.search.onkeyup = () => {
+      if (this.search.value.length == 0) {
+        this.list.innerHTML = '';
+        return;
+      }
+      const script = document.createElement('script');
+      script.src = "https://www.baidu.com/su?wd=" + this.search.value + "&cb=jsonp.showMsg";
+      this.body.appendChild(script);
+      this.body.removeChild(script);
+    }
+  };
+  showMsg(msg) {
+    var href = $('#form').attr('action');
+    var name = $('#inputText').attr('name');
+    var v = $('#inputText').val();
+    var str = '';
+    for (var i = 0; i < msg.s.length; i++) {
+      var sk = new Array();
+      sk[i] = msg.s[i].replace(/\s*/g, ''); //去掉关键字空格
+      str += '<a href=' + href + '?' + name + '=' + sk[i] + '><li><span>' + (i + 1) + '</span>' + msg.s[i] + '</li></a>';
+    }
+    this.list.innerHTML = str;
+    if (str) { //有返回才显示#searchlist
+      $('#searchlist').css("display", "block");
+    }
+    document.onkeydown = function(event) {
+      if (event.keyCode == 8 && v.length == 1) {
+        $('#searchlist').css("display", "none");
+      }
+    };
+    $(document).click(function() { //点击其他地方隐藏#searchlist
+      $('#searchlist').css("display", "none");
+    });
+  }
+}
+const jsonp = new searchHint();
+
+//导航、引擎选择器结束
+
+/*
+ * ALL RIGHTS RESERVED.
+ *
+ * 作者：酷安@_小K同學
+ * 项目开始日期：2020年01月26日
+ * 上次修改时间：2020年03月24日
+ * 开发日志：https://kksan.top/posts/12023
+ *
+ * 开源相关：
+ * Github：https://github.com/Jackie1123/aNavigation
+ * CSSFX：https://cssfx.netlify.com
+ * jQuery：https://jquery.com
+ * slideout：https://slideout.js.org
+ * bootstrap：https://getbootstrap.com
+ * font-awesome：https://fontawesome.com
+ *
+ * 版权所有，请勿删除！
+ */
